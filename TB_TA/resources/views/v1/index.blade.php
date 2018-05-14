239e62585702d7ba5336d589013ab60700a431f7
YudiMuchlisin <yudi.muchlisin1@gmail.com>
2018-05-14 16:06:59 +0700

    tambah asset di directory gbr

--- TB_TA/resources/views/v1/index.blade.php
+++ TB_TA/resources/views/v1/index.blade.php
@@ -66,7 +66,7 @@
                   <li><!-- start message -->
                     <a href="#">
                       <div class="pull-left">
-                        <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Support Team
@@ -138,13 +138,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
 
                 <p>
                   Alexander Pierce - Web Developer
@@ -195,7 +195,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Alexander Pierce</p>
@@ -423,14 +423,14 @@
           </div>
         </div>
         <div class="box-body">
-          
+
          <div>
           @if (\Session::has('success'))
         <div class="alert alert-success">
           <p>{{ \Session::get('success') }}</p>
         </div><br/>
         @endif
-        </div> 
+        </div>
         <table class="table table-striped">
           <thead>
           <tr>
@@ -445,7 +445,7 @@
           </thead>
         <tbody>
           @foreach($TugasAkhir as $TugasAkhir)
-          <tr>           
+          <tr>
             <td>{{$TugasAkhir['NIM']}}</td>
             <td>{{$TugasAkhir['Nama_Mahasiswa']}}</td>
             <td>{{$TugasAkhir['Jenis_Kelamin']}}</td>
@@ -466,7 +466,7 @@
           </tr>
         @endforeach
       </tbody>
-    </table>        
+    </table>
 
         </div>
         <!-- /.box-body -->
