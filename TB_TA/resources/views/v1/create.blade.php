6827200f393cc5697524a670dfb31f5c5389283c
YudiMuchlisin <yudi.muchlisin1@gmail.com>
2018-05-14 16:41:17 +0700

    Edit + tambah data

--- TB_TA/resources/views/v1/create.blade.php
+++ TB_TA/resources/views/v1/create.blade.php
@@ -66,7 +66,7 @@
                   <li><!-- start message -->
                     <a href="#">
                       <div class="pull-left">
-                        <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Support Team
@@ -138,13 +138,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
 
                 <p>
                   Alexander Pierce - Web Developer
@@ -195,7 +195,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="{{asset('AdminLTE-2.4.3/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Alexander Pierce</p>
@@ -423,7 +423,7 @@
           </div>
         </div>
         <div class="box-body">
-          
+
          <div>
             @if ($errors->any())
             <div class="alert alert-danger">
@@ -439,8 +439,8 @@
           <p>{{ \Session::get('success') }}</p>
         </div><br/>
         @endif
-        </div> 
-        
+        </div>
+
             <form method="post" action="{{url('v1')}}" enctype = "multipart/form-data">
               {{csrf_field()}}
               <div class="box-body">
@@ -461,9 +461,9 @@
                   </div>
                   <div class="radio">
                     <label>
-                      <input type="radio" name="Jenis_Kelamin" id="Jenis_Kelamin" value="Perempuan">Perempuan                      
+                      <input type="radio" name="Jenis_Kelamin" id="Jenis_Kelamin" value="Perempuan">Perempuan
                     </label>
-                  </div>                  
+                  </div>
                 </div>
 
                 <div class="form-group">
