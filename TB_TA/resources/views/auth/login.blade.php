03297bc347256e3943a375547d56645e3156d1e4
raka <raka.ilham46@gmail.com>
2018-05-15 19:32:05 +0700

    a

--- TB_TA/resources/views/auth/login.blade.php
+++ TB_TA/resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Tugas</b>Akhir</a>
+    <a href="#"><b>Tugas</b>Akhir</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
